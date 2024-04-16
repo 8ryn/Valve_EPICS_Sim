a9c711063e4ed2b2d436fb957edf865ef5d15760
Bryn Roberts <bar@observatorysciences.co.uk>
2024-04-16 10:28:21 +0100

    Implementation of output PVs in IOC

--- ads-ioc/iocBoot/iocvalve/st.cmd
+++ ads-ioc/iocBoot/iocvalve/st.cmd
@@ -11,11 +11,30 @@ cd "${TOP}"
 dbLoadDatabase "dbd/valve.dbd"
 valve_registerRecordDeviceDriver pdbbase
 
+# Set local AMS net ID
+AdsSetLocalAMSNetID("192.168.1.7.1.1")
+#AdsSetLocalAMSNetID("127.0.0.1.1.1")
+
+epicsEnvSet("PREFIX_VALVEDEMO", "VALVE-DEMO-01")
+epicsEnvSet("PORT_VALVEDEMO", "valve-demo-port")
+epicsEnvSet("IP_VALVEDEMO", "127.0.0.1")
+epicsEnvSet("AMS_ID_VALVEDEMO", "127.0.0.1.1.1")
+
 ## Load record instances
-#dbLoadRecords("db/valve.db","user=bar")
+dbLoadRecords("db/valve.db","P=$(PREFIX_VALVEDEMO), PORT=$(PORT_VALVEDEMO)")
+
+# Open ADS port
+# AdsOpen(port_name,
+#         remote_ip_address,
+#         remote_ams_net_id,
+#         sum_buffer_nelem (default: 500),
+#         ads_timeout (default: 500 ms),
+#         device_ads_port (default: 851),
+#         sum_read_period (default: 1 ms))
+AdsOpen("$(PORT_VALVEDEMO)", "$(IP_VALVEDEMO)", "$(AMS_ID_VALVEDEMO)", "500", "500", "48898")
+
+# Enable asyn trace output for errors and warnings
+asynSetTraceMask("$(PORT_VALVEDEMO)", 0, 0x21)
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
-
-## Start any sequence programs
-#seq sncxxx,"user=bar"
